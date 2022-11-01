13dbdb2ef58e596ddfd1d3c5b5e694c5ebd6c72c
Josiah_L <sjosiahleon@gmail.com>
2022-11-01 11:10:28 -0700

    created footer

---
+++ src/components/footer/foot.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.scss";
+
+
+const Footer = () => {
+
+    return (
+        <div>
+            this is the footer
+        </div>
+    )
+}
+
+export default Footer;
