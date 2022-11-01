f527cc7ea484b0b21ad9415fa70be4f722efb98e
Josiah_L <sjosiahleon@gmail.com>
2022-11-01 10:44:29 -0700

    create navbar component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React, {useEffect, UseState} from "react";
+import "./navbar.scss";
+
+
+const Navbar = () => {
+
+    
+    return (
+        <div className="navbar">
+            this is the nav bar it does cool things
+        </div>
+    )
+};
+
+export default Navbar;
