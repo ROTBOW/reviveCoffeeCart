0d555e8492950ee4e7645607b748bce9916315d3
Josiah_L <sjosiahleon@gmail.com>
2022-12-10 08:22:57 -0800

    Made phone button a one click for mobile Updated css for svg buttons to not have a on hover when moblie

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import "./navbar.scss";
+import React, { useState, useEffect } from "react";
 import logo from '../../assets/images/rccLogo.png';
+import "./navbar.scss";
 
 import {ReactComponent as Phone} from '../../assets/images/phone.svg';
 import {ReactComponent as Email} from '../../assets/images/email.svg';
 import {ReactComponent as Insta} from '../../assets/images/instagram.svg';
 import {ReactComponent as Copy} from '../../assets/images/copy.svg';
 
-
-const Navbar = () => {
+const PhoneButton = () => {
+    const [width, setWidth] = useState(window.innerWidth);
     const [showPhone, setShowPhone] = useState(false);
     const [copied, setCopied] = useState(false);
 
@@ -21,6 +21,38 @@ const Navbar = () => {
         navigator.clipboard.writeText('951-776-7850');
     }
 
+    const handleWindowResize = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+        return () => window.removeEventListener('resize', handleWindowResize)
+    }, [])
+
+    const isMobile = width <= 700;
+
+    if (!isMobile) {
+        return (
+            <li>
+                <Phone onClick={()=>{setShowPhone(val => !val)}}/>
+                <div id="phone" className={showPhone ? '' : 'hide'}>
+                    <a href="tel: 951-776-7850">(951) 776-7850</a> <Copy onClick={handleClick}/>
+                    { copied ? <spam>Copied!</spam> : ''}
+                </div>
+            </li>
+        )
+    } else {
+        return (
+            <li>
+                <a href="tel: 951-776-7850"><Phone/></a>
+            </li>
+        )
+    }
+};
+
+const Navbar = () => {
+
     return (
         <div className="navbar">
             <div>
@@ -32,13 +64,7 @@ const Navbar = () => {
                 <h4>Events & Meetings</h4>
                 <div style={{"position": "relative"}}>
                     <ul>
-                        <li>
-                            <Phone onClick={()=>{setShowPhone(val => !val)}}/>
-                            <div id="phone" className={showPhone ? '' : 'hide'}>
-                                <a href="tel: 951-776-7850">(951) 776-7850</a> <Copy onClick={handleClick}/>
-                                { copied ? <spam>Copied!</spam> : ''}
-                            </div>
-                        </li>
+                        <PhoneButton/>
                         <li><a href="mailto: revivecoffeecart@gmail.com"><Email/></a></li>
                         <li><a href="https://www.instagram.com/revivecoffeecart/" target="_blank" rel="noreferrer"><Insta/></a></li>
                     </ul>
