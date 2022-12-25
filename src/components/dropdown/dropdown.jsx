99db1b6aabc2cbae67e091146426a85d88ab85ac
Josiah Leon <sjosiahleon@gmail.com>
2022-12-25 12:19:27 -0800

    Refactor - Removed dropdowns effects

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -1,25 +1,16 @@
-// import {ReactComponent as Arrow} from '../../assets/images/caret.svg';
 import React from "react";
 import './dropdown.scss';
 
 const Dropdown = props => {
-    // const [drop, setDrop] = useState(true);
-    const drop = true;
-
     return (
-
         <div className="dropdown-container">
-            {/* <div className="dropdown-button" onClick={() => setDrop(val => !val)}> */}
             <div className="dropdown-button">
-                {/* <Arrow className={drop ? 'active' : ''}/> */}
-                <div className={`side-line ${drop ? 'active': ''}`}>-</div>
                 <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    {props.text}
+                    -{props.text}-
                     <div className="dropdown-subtext">{props.subtext}</div>
                 </div>
-                <div className={`side-line ${drop ? 'active': ''}`}>-</div>
             </div>
-            <ul className={drop ? '' : 'hide'}>
+            <ul>
                 {
                     props.list.split(',').map((item, idx) => (<li key={idx}>{item}</li>))
                 }
